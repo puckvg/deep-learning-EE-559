7471a806f3801fc8496eb86d73ba837a64e229b1
Puck van Gerwen <3156622-puck_vg@users.noreply.gitlab.com>
2021-03-11 21:17:12 +0100

    still not right and don't know why

--- practical_3.py
+++ practical_3.py
@@ -12,17 +12,10 @@ def dsigma(x):
 
 
 def loss(v, t): 
-    # v is the predicted tensor 
-    # t is the target one 
-    # return l2-norm squared i.e. sum of the squares
     return torch.sum(torch.pow((t - v), 2))
 
 
 def forward_pass(w1, b1, w2, b2, x): 
-    # input vector x 
-    # weight and bias of 2 layers 
-    # returns tuple x0, s1, x1, s2, x2
-
     s1 = torch.mv(w1, x) + b1
     x1 = sigma(s1)
 
@@ -42,7 +35,7 @@ def backward_pass(w1, b1, w2, b2,
     # tensors used to store cumulated sums of gradient on individual samples 
     # updates latters based on formula of backward pass 
 
-    dl_dx2 = -2*(x2-t)
+    dl_dx2 = -2*(t-x2)
     dl_ds2 = dl_dx2 * dsigma(s2)
     dl_dw2_i = torch.mm(dl_ds2.view(dl_ds2.shape[0], 1), x1.view(1, x1.shape[0]))
 
@@ -54,7 +47,7 @@ def backward_pass(w1, b1, w2, b2,
     dl_db1_i = dl_ds1 
     dl_db2_i = dl_ds2
 
-
+    # update 
     dl_dw1 += dl_dw1_i
     dl_dw2 += dl_dw2_i
     dl_db1 += dl_db1_i
@@ -65,29 +58,37 @@ def backward_pass(w1, b1, w2, b2,
 
 def step(x, t,
         w1, b1, w2, b2, 
-        dl_dw1, dl_db1, dl_dw2, dl_db2,
-        step_size): 
+        dl_dw1, dl_db1, dl_dw2, dl_db2): 
     # forward pass 
     x0, s1, x1, s2, x2 = forward_pass(
             w1, b1, w2, b2, x)
 
     # backward pass
-    dl_dw1, dl_db1, dl_dw2, dl_db2 =\
-            backward_pass(
+    dl_dw1, dl_db1, dl_dw2, dl_db2 = backward_pass(
                     w1, b1, w2, b2,
                     t, 
                     x0, s1, x1, s2, x2, 
                     dl_dw1, dl_db1, dl_dw2, dl_db2
                     )
-    # update in memory of initial params 
-
-    # update params 
-    w1 -= step_size * dl_dw1
-    w2 -= step_size * dl_dw2
-    b1 -= step_size * dl_db1
-    b2 -= step_size * dl_db2
     
-    return w1, b1, w2, b2, dl_dw1, dl_dw2, dl_db1, dl_db2
+    return dl_dw1, dl_dw2, dl_db1, dl_db2
+
+
+def predict_score(x_test, t_test, w1, b1, w2, b2):
+    predictions = torch.empty(len(x_test),n_layer_2)
+    for i in range(len(x_test)):
+        _, _, _, _, pred = forward_pass(w1, b1, w2, b2, x_test[i])
+
+    pred_index = torch.argmax(predictions, dim=-1)
+
+    # compare to true value 
+    test_index = torch.argmax(t_test, dim=-1)
+
+    # get number of nonzero (incorrect)
+    incorrect = torch.count_nonzero(test_index - pred_index)
+    # percentage correct 
+    score = (len(x_test) - incorrect) / len(x_test) 
+    return score
 
 
 if __name__ == "__main__":
@@ -113,38 +114,32 @@ if __name__ == "__main__":
     b2 = torch.empty(n_layer_2).normal_(mean=0, std=1e-6)
 
     for s in range(10):
+        print('iter', s)
         # create tensors to sum up gradients 
         dl_dw1 = torch.zeros(w1.shape)
         dl_db1 = torch.zeros(b1.shape)
         dl_dw2 = torch.zeros(w2.shape)
         dl_db2 = torch.zeros(b2.shape)
 
+        # update derivatives for all training data
         for i in range(len(x)):
-            w1, b1, w2, b2, dl_dw1, dl_dw2, dl_db1, dl_db2 =\
-             step(
+            dl_dw1, dl_dw2, dl_db1, dl_db2 = step(
                 x[i], t[i], 
                 w1, b1, w2, b2, 
-                dl_dw1, dl_db1, dl_dw2, dl_db2, 
-                step_size
+                dl_dw1, dl_db1, dl_dw2, dl_db2
                 )
 
+        # update params for average derivatives over training data
+        w1 -= step_size * dl_dw1
+        w2 -= step_size * dl_dw2
+        b1 -= step_size * dl_db1
+        b2 -= step_size * dl_db2
 
-    predictions = torch.empty(len(x_test),n_layer_2)
-    for i in range(len(x_test)):
-        _, _, _, _, pred = forward_pass(w1, b1, w2, b2, x_test[i])
+        train_score = predict_score(x, t, w1, b1, w2, b2)
+        print('train score', train_score)
+        test_score = predict_score(x_test, t_test, w1, b1, w2, b2)
+        print('test score', test_score)
 
-    # prediction is argmax along second dimension 
-    pred_index = torch.argmax(predictions, dim=-1)
-    # these correspond to to 0 to 9 
 
-    # compare to true value 
-    test_index = torch.argmax(t_test, dim=-1)
-    # train error 
-
-    # test error 
-    # get number of nonzero (incorrect)
-    incorrect = torch.count_nonzero(test_index - pred_index)
-    # 
-    print('n incorrect', incorrect)
 
 
